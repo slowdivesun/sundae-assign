4f2acd700f232bb4b6df145bf4649141ea551bb5
Shivam <shivam.gup82@gmail.com>
2023-07-06 23:45:05 +0530

    Testing styles

--- insta-feed/app/insta/page.js
+++ insta-feed/app/insta/page.js
@@ -1,9 +1,9 @@
 import InstaSection from "../../components/InstaSection";
-import styles from "insta.module.css";
+import styles from "./insta.module.css";
 
 const InstaPage = () => {
   return (
-    <div class='bg-insta'>
+    <div className={styles["bg-insta"]}>
       <h1>Welcome</h1>
       <div>
         <InstaSection />
