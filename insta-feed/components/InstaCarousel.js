ec03fb5e747906a98b0e074eb05b58ef25f6a834
Shivam <shivam.gup82@gmail.com>
2023-07-07 10:06:08 +0530

    Changed basic display API call

--- insta-feed/components/InstaCarousel.js
+++ insta-feed/components/InstaCarousel.js
@@ -1,16 +1,14 @@
+"use client";
 import Carousel from "react-material-ui-carousel";
 import { Paper, Button } from "@mui/material";
 
-const InstaHeader = ({link}) => {
+const InstaHeader = ({ link }) => {
   return (
-    return (
-        <Carousel>
-          {items.map((item, i) => (
-            <Item key={i} item={item} />
-          ))}
-        </Carousel>
-      );
-      
+    <Carousel>
+      {items.map((item, i) => (
+        <Item key={i} item={item} />
+      ))}
+    </Carousel>
   );
 };
 
