8483aa78426293182549907fe435e0e3f3970a46
Shivam <shivam.gup82@gmail.com>
2023-07-07 09:43:25 +0530

    Changed MUI imports

--- insta-feed/app/insta/page.js
+++ insta-feed/app/insta/page.js
@@ -5,9 +5,6 @@ import styles from "./insta.module.css";
 const InstaPage = () => {
   return (
     <div className={styles["bg-insta"]}>
-      <Typography variant='h3' gutterBottom>
-        Welcome
-      </Typography>
       <div>
         <InstaSection />
       </div>
---
+++ insta-feed/components/InstaHeader.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { Typography } from "@mui/material";
+
+const LoginButton = () => {
+  return (
+    <Typography variant='h3' gutterBottom>
+      Welcome
+    </Typography>
+  );
+};
+
+export default LoginButton;
