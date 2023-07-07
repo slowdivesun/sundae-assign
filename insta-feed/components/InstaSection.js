18d1cae1e7c9e239f8500d6740caf038d589707e
Shivam <shivam.gup82@gmail.com>
2023-07-07 12:58:19 +0530

    Changed basic display API call

--- insta-feed/components/InstaSection.js
+++ insta-feed/components/InstaSection.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import InstaCarousel from "./InstaCarousel";
 
 const InstaSection = () => {
@@ -116,6 +116,8 @@ const InstaSection = () => {
           );
         } else if (post.media_type === "CAROUSEL_ALBUM") {
           return <InstaCarousel key={i} post={post} />;
+        } else {
+          return <React.Fragment></React.Fragment>;
         }
       })}
       yuhh
